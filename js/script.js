29afe70c1cf3ffc63fd034e4094bab86eba0096e
DaveBeaut <117293937+DaveBeaut@users.noreply.github.com>
2023-01-31 09:00:33 +0100

    #

--- js/script.js
+++ js/script.js
@@ -1,57 +1,45 @@
-window.onload = function() {
+addEventListener("load", (event) => {
 
-// nAPIS à itérer
-  let nAPIS = [1,2];
+  let nApis = [1,2];
 
-    // Faire autant de fois de fetch qu'il a d'APIS (retourne API) et modifie le dernier caractère de l'adresse => appeler les fonctions si res.ok sinon gestion des erreurs.
-  nAPIS.forEach(function(API) {
-    fetch(`https://www.tbads.eu/greta/kercode/ajax/?article=${API}`)
+  nApis.forEach(function(api) {
+    fetch(`https://www.tbads.eu/greta/kercode/ajax/?article=${api}`)
         .then(res => { 
-            console.log(res)
 
             if(res.ok){
+                
                 res.json().then(data => {
-                    console.log(data);
-                    updateJumbo(API, data);
-                    updateModal(API, data);
+                    updateJumbo(api, data);
+                    updateModal(api, data);
                 })
 
-            } else {
-                console.log('ERREUR D\'API');
-            }
+                } else {console.log('ERREUR D\'API');}
         })
         
-        .catch(error => {
-            console.error('ERREUR : ', error);
-        })
+        .catch(error => {console.error('ERREUR : ', error);})
 
   });
-  
-                // ! Utiliser des variables génériques terminant par un chiffre itérable !
 
-                // FONCTION MODIF DES JUMBOS 
-function updateJumbo(API, data) {
-    // décla des variables relativement aux IDs des Jumbos
-    let dateNews = document.getElementById(`dateNews${API}`);
-    let jumboP = document.getElementById(`jumbo${API}P`);
+    // FONCTION MODIF DES JUMBOS 
+    function updateJumbo(api, data) {
+        let dateNews = document.getElementById(`dateNews${api}`);
+        let jumboP = document.getElementById(`jumbo${api}P`);
 
-    // modifs de l'HTML
-    dateNews.innerHTML = `${data.date.day} ${data.date.month} ${data.date.year}`;
-    jumboP.innerHTML = data.content[0];
-}
+        dateNews.innerHTML = `${data.date.day} ${data.date.month} ${data.date.year}`;
+        jumboP.innerHTML = data.content[0];
+    }
 
-                // FONCTION MODIF DES BOITES MODALES
-function updateModal(API, data) {
-    // décla des variables relativement aux IDs des boîtes modales
-    let imgJumbo = document.getElementById(`imgJumbo${API}`);
-    let auteurDate = document.getElementById(`auteurDate${API}`);
-    let texteBM = document.getElementById(`texteBM${API}`);
-    let modalTitle = document.getElementById(`modalTitle${API}`);
+    // FONCTION MODIF DES BOITES MODALES
+    function updateModal(api, data) {
+        let imgJumbo = document.getElementById(`imgJumbo${api}`);
+        let auteurDate = document.getElementById(`auteurDate${api}`);
+        let texteBM = document.getElementById(`texteBM${api}`);
+        let modalTitle = document.getElementById(`modalTitle${api}`);
 
-    // modifs de l'HTML
-    modalTitle.innerHTML = data.title;
-    imgJumbo.src = data.picture;
-    auteurDate.innerHTML = `${data.author.name} ${data.author.surname} | ${data.date.day} ${data.date.month} ${data.date.year}`;
-    texteBM.innerHTML = data.content[0];
-}
-};
+        modalTitle.innerHTML = data.title;
+        imgJumbo.src = data.picture;
+        auteurDate.innerHTML = `${data.author.name} ${data.author.surname} | ${data.date.day} ${data.date.month} ${data.date.year}`;
+        texteBM.innerHTML = data.content[0];
+    }
+
+});
